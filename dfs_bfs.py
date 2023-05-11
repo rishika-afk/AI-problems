002d68353c2bcaa4856bb8cb398696d8ad1d2044
Rishika Gupta <73276425+rishika-afk@users.noreply.github.com>
2023-05-11 23:18:54 +0530

    Create dfs_bfs.py

---
+++ dfs_bfs.py
@@ -0,0 +1,43 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+
+visited_bfs = []
+queue = []
+
+
+def bfs(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+
+        for neighbour in graph[s]:
+            if neighbour not in visited:
+                visited.append(neighbour)
+                queue.append(neighbour)
+
+
+visited_dfs = set()
+
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print(node, end=" ")
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+print("DFS:", end=" ")
+dfs(visited_dfs, graph, 'A')
+print('\n')
+print("BFS:", end=" ")
+bfs(visited_bfs, graph, 'A')
+print('\n')
